6e0a701bfde340f00dae0f2da8331f7734b12006
SG_wang <wsg20110828@163.com>
2018-04-13 20:08:30 +0800

    add read me

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-# kaggle-start
+# 如何在半年的时间里，零基础入门机器学习?
+
+- 3月份
+
+    - 搭建机器学习环境
+    - 注册Kaggle账号，登录Kaggle平台
+    - 加入第一个Kaggle比赛Titanic
+
+- 4月份
+
+    - 初试Titanic
+    - 机器学习的基本流程
+    - 数据处理，特征工程
+    - XGBoost解决Titanic问题
+
+- 5月份
+
+    - 初试House prediction竞赛
+    - 数据可视化
+    - 数据清洗
+    - 特征工程
+    - 模型评估
+    - Kaggle终极杀器：模型融合
+
+- 6月份
+
+    - Logistic回归识别手写数字
+    - 深度学习解决图像识别问题
+    - Tensorflow、Keras识别手写数字
+    - 深度学习模型训练技巧分享
+
+- 7月份
+
+- Kaggle实战拿银
 
 分享Kaggle的入门教程.
 
@@ -6,10 +39,4 @@
 
 ### Titanic竞赛
 
-- [0-3 Kaggle竞赛入门 - Titanic实战](https://github.com/wsg011/kaggle-start/blob/master/titanic/Notebook/0-3%20kaggle%20start%20with%20Titanic.ipynb)
-
-## 说明
-
-- titanic：Titanic幸存者预测竞赛
-- digit-recognizer：手写数字识别竞赛
-- house-predict：房价预测竞赛
+- [0-3 Kaggle竞赛入门 - Titanic实战](titanic/Notebook/0-3_kaggle_start_with_Titanic.ipynb)
--- titanic/Code/ensemble_xgb_rf.py
+++ titanic/Code/ensemble_xgb_rf.py
@@ -2,13 +2,11 @@
 
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
 
 from xgboost import XGBClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import BaggingClassifier
-
-from sklearn.model_selection import cross_val_score
 
 
 def stacking(alg, x_train, y_train, test):
@@ -31,15 +29,16 @@ def submit(alg, test_data):
 if __name__ == '__main__':
     train = pd.read_csv('../data/feature_train.csv')
     test = pd.read_csv('../data/feature_test.csv')
-    print train.head()
+    print(train.head())
 
     features = ["Pclass", "Fare", "Age", "SibSp", "child", "Parch", "sex", "fimalysize",
                 "embark", "name", 'cabin']
     x_train = train[features]
     y_train = train["Survived"]
-    print x_train.info()
+    print(x_train.info())
 
-    rf = RandomForestClassifier(n_estimators=140, max_depth=4, min_samples_split=6, min_samples_leaf=4, n_jobs=4)
+    rf = RandomForestClassifier(
+        n_estimators=140, max_depth=4, min_samples_split=6, min_samples_leaf=4, n_jobs=4)
     rf_scores = cross_val_score(rf, x_train, y_train, cv=3)
 
     dt = DecisionTreeClassifier()
@@ -55,9 +54,9 @@ if __name__ == '__main__':
     bagging_scores = cross_val_score(bagging_clf, x_train, y_train, cv=3)
     bagging_clf.fit(x_train, y_train)
 
-    print "rf scores:", rf_scores.mean()
-    print "dt scores:", dt_scores.mean()
-    print "xgb scores:", xgb_scores.mean()
-    print "bagging scores:", bagging_scores.mean()
+    print("rf scores:", rf_scores.mean())
+    print("dt scores:", dt_scores.mean())
+    print("xgb scores:", xgb_scores.mean())
+    print("bagging scores:", bagging_scores.mean())
 
     submit(xgb, test)
